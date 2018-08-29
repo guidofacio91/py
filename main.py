dd9486de35251a37ac6efe5a6ae95189318d8a40
Juan Guido Facio <juanguidofacio@Guido.local>
2018-08-28 21:37:29 -0300

    subo main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+print("hola papu")
+
+entero=1
+
+
+#IFFFFFF
+if 1==entero:
+  print("1 es igual a 1")
+
+lista = ["h","o","l","a"]
+
+
+#FORRRR
+for x in lista:
+  print(x)
+
+print("visual studio code")
+print("w3school pyhon tutorial")
+
+print("codeacademy syllabus")
